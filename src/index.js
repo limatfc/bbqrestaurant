31e6d014e7a751590cb1799c2f2e1be99e02cdd1
Thaisa Lima <thaisafc.lima@outlook.com>
2022-04-12 18:48:34 +0200

    starting

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from "react-dom/client";
+import TasksProvider from "./store/tasks-provider";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <TasksProvider>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </TasksProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
