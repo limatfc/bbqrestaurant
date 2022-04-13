b33a618613db831b5bcfad01b807f1c201e305b5
Thaisa Lima <thaisafc.lima@outlook.com>
2022-04-13 17:48:44 +0200

    set up the route

--- src/components/client/Category.jsx
+++ src/components/client/Category.jsx
@@ -1,3 +1,14 @@
+import { useParams } from "react-router-dom";
+import useReadData from "../../hooks/useReadData";
+import useDataProvider from "../../store/useDataProvider";
+
 export default function Category() {
+  const { category } = useParams();
+  const dataContext = useDataProvider();
+  const { productsHandler, products } = dataContext;
+  const { status } = useReadData(productsHandler, "products/product/chicken");
+
+  console.log(products);
+
   return <div>Category</div>;
 }
--- src/components/client/CategoryCard.jsx
+++ src/components/client/CategoryCard.jsx
@@ -1,12 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
 export default function CategoryCard({ item }) {
+  const navigate = useNavigate();
   const { description, imageDescription, imageURL, name } = item;
+
+  function onClickHandler() {
+    navigate(`/menu/${name}`);
+  }
 
   return (
     <div className="category-card-wrapper">
       <img src={imageURL} alt={imageDescription} />
       <h3>{name}</h3>
       <p>{description}</p>
-      <button>View more</button>
+      <button onClick={onClickHandler}>View more</button>
     </div>
   );
 }
